46dee09882f76025dd72a96cd005b5310347f1b4
Alphabetus <contact@followarmy.com>
2018-11-26 23:53:03 +0100

    v1.0.5

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,36 @@
 <head>
+<!-- LICENSE
+  NOMADS - MMORPG - BROWSER GAME
+    Copyright (C) 2018  Diogo Gomes
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
+
+  Apache HTTP Server
+    Copyright 2016 The Apache Software Foundation.
+
+    This product includes software developed at
+    The Apache Software Foundation (http://www.apache.org/).
+
+    Portions of this software were developed at the National Center
+    for Supercomputing Applications (NCSA) at the University of
+    Illinois at Urbana-Champaign.
+
+    This software contains code derived from the RSA Data Security
+    Inc. MD5 Message-Digest Algorithm, including various
+    modifications by Spyglass Inc., Carnegie Mellon University, and
+    Bell Communications Research, Inc (Bellcore).
+ -->
 <!-- custom CSS -->
 <link rel="stylesheet" type="text/css" href="theme/normal.css">
 <!-- Global site tag (gtag.js) - Google Analytics -->
--- index.php
+++ index.php
@@ -11,10 +11,6 @@ include "common/common_functions.php";
 include "includes/dbConfig.php";
 ?>
 <html>
-<!--
-Open Source Browser Game ~ Space Exploration
-Nomads @ 2018. | by Alphabetus
--->
 <?php include "includes/head.php" ?>
 <body>
   <div id="topper">
